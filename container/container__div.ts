6c734b821751cda04f3c245d31d01d63f88f19ae
Brian Takita <brian.takita@gmail.com>
2024-02-25 14:27:07 -0500

    ./container: container__div_: + class?:string
    ./icon: + heroicons_user_circle_

--- container/container__div.ts
+++ container/container__div.ts
@@ -1,15 +1,20 @@
 import { class_ } from 'ctx-core/html'
 import { relement_env_T, type tag_dom_T } from 'relementjs'
 import { div_ } from 'relementjs/html'
-export function container__div_<env_T extends relement_env_T>(...children:tag_dom_T[]) {
-  return (
+export function container__div_<env_T extends relement_env_T>({
+	class: _class
+}:{
+	class?:string
+}, ...children:tag_dom_T[]) {
+	return (
 		div_<env_T>({
 			class: class_(
 				'max-w-7xl',
 				'mx-auto',
 				'px-6',
 				'md:px-12',
-				'xl:px-6')
+				'xl:px-6',
+				_class)
 		}, ...children)
 	)
 }
